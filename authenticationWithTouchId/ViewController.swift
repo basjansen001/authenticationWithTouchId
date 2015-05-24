d9e0430edd65b24f6867a226991fb56b440067a2
Bas Jansen <basjansenapple@gmail.com>
2015-05-24 20:45:08 +0200

    Update ViewController.swift

--- authenticationWithTouchId/ViewController.swift
+++ authenticationWithTouchId/ViewController.swift
@@ -11,37 +11,36 @@ import LocalAuthentication
 
 class ViewController: UIViewController {
 
-func authenticateUserWithTouchId() {
-    var myContext = LAContext()
-    var error: NSError?
-    var myLocalizedReasonString = "For authentication purposes, we need to authenticate you with your fingermark!"
+  func authenticateUserWithTouchId() {
+      var myContext = LAContext()
+      var error: NSError?
+      var myLocalizedReasonString = "For authentication purposes, we need to authenticate you with your fingermark!"
 		
-    if myContext.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &error) {
-        myContext.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString, reply: { (var succes: Bool, var error: NSError?) -> Void in
-        if succes {
-            // Succesfully authenticated
-        } else {
-            // User did not authenticate succesfully, look at the error and take appropriate action
-        }
-        })
-    } else {
-        // Could not evelaute policy; look at authError and present an appropriate message to user.
+      if myContext.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &error) {
+          myContext.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString, reply: { (var succes: Bool, var error: NSError?) -> Void in
+          if succes {
+              // Succesfully authenticated
+          } else {
+              // User did not authenticate succesfully, look at the error and take appropriate action
+          }
+          })
+      } else {
+          // Could not evelaute policy; look at authError and present an appropriate message to user.
+      }
+   }
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
- }
-	
-	
-  override func viewDidLoad() {
-      super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
-  }
 
-  override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-  }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
   
-  @IBAction func authenticate() {
-      self.authenticateUserWithTouchId()
-  }
+    @IBAction func authenticate() {
+        self.authenticateUserWithTouchId()
+    }
 }
 
