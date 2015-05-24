17dada14b4e13ee6b218b2048913c03da257de0c
Bas Jansen <basjansen14@outlook.com>
2015-05-24 20:31:01 +0200

    Initial Commit

---
+++ authenticationWithTouchId/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  authenticationWithTouchId
+//
+//  Created by Bas Jansen on 24-05-15.
+//  Copyright (c) 2015 Bas Jansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
