a33b4c82128eabd2e049ab1bea3af024389ccd63
Bas Jansen <basjansenapple@gmail.com>
2015-05-24 20:44:01 +0200

    Update ViewController.swift

--- authenticationWithTouchId/ViewController.swift
+++ authenticationWithTouchId/ViewController.swift
@@ -10,38 +10,38 @@ import UIKit
 import LocalAuthentication
 
 class ViewController: UIViewController {
-	
-	func authenticateUserWithTouchId() {
-		var myContext = LAContext()
-		var error: NSError?
-		var myLocalizedReasonString = "Om in te kunnen loggen moet u dit met uw vingerafdruk doen!"
-		
-		if myContext.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &error) {
-			myContext.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString, reply: { (var succes: Bool, var error: NSError?) -> Void in
-				if succes {
-					// Succesfully authenticated
-				} else {
-					// User did not authenticate succesfully, look at the error and take appropriate action
-				}
-			})
-		} else {
-			// Could not evelaute policy; look at authError and present an appropriate message to user.
-		}
-	}
-	
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
+func authenticateUserWithTouchId() {
+    var myContext = LAContext()
+    var error: NSError?
+    var myLocalizedReasonString = "For authentication purposes, we need to authenticate you with your fingermark!"
+		
+    if myContext.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &error) {
+        myContext.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString, reply: { (var succes: Bool, var error: NSError?) -> Void in
+        if succes {
+            // Succesfully authenticated
+        } else {
+            // User did not authenticate succesfully, look at the error and take appropriate action
+        }
+        })
+    } else {
+        // Could not evelaute policy; look at authError and present an appropriate message to user.
+    }
+ }
 	
-	@IBAction func authenticate() {
-		self.authenticateUserWithTouchId()
-	}
+	
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func authenticate() {
+      self.authenticateUserWithTouchId()
+  }
 }
 
